5878450b852b1d8864388f57537f39ea12f7243d
Mamta Pandey <loginmamta1910@gmail.com>
2023-06-06 22:02:56 +0800

    remove unwanted files

--- backenedTS/output/controllers/getAllProducts.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAllProducts = void 0;
-const getAllProducts = (req, res, next) => {
-    res.send('get all products have been called');
-};
-exports.getAllProducts = getAllProducts;
